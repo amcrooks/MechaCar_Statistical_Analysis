b2f0d3a37de3f52ae88be671e79194d7046d5259
Alexis Crooks <lexiec@Alexiss-MBP.lan>
2022-02-15 22:09:31 -0600

    linear

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,2 @@
+lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=MechaCar) #generate multiple linear regression model
+summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg,data=MechaCar)) #generate multiple linear regression model

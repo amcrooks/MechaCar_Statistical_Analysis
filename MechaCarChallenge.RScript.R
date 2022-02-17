d706ebc1e1ce937df1852466da815150e24ecfe7
Alexis Crooks <lexiec@Alexiss-MacBook-Pro.local>
2022-02-17 13:31:30 -0600

    stats

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,4 +1,13 @@
+#Deliverable 1
+#read file
 MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 library(dplyr)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar)) #generate multiple linear regression model
+
+#Deliverable 2
+#read file
+Suspension <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+#create summary table
+total_summary <- Suspension %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Mediaon=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table

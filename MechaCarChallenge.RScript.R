5e4938e1e8ae16b6563b0509eadab52c0c510b90
Alexis Crooks <lexiec@Alexiss-MacBook-Pro.local>
2022-02-19 15:05:32 -0600

    t.test

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -11,3 +11,8 @@ Suspension <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors
 #create summary table
 total_summary <- Suspension %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Mediaon=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table
+#compare lots to population mean
+t.test(Suspension$PSI, mu=1500)
+t.test(subset(Suspension$PSI, Suspension$Manufacturing_Lot == "Lot1"), mu=1500)
+t.test(subset(Suspension$PSI, Suspension$Manufacturing_Lot == "Lot2"), mu=1500)
+t.test(subset(Suspension$PSI, Suspension$Manufacturing_Lot == "Lot3"), mu=1500)
